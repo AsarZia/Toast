cd9b83ecd10a0f288cd05cda9dc2aa3ad17dcc09
Asar Zia <asarzia45@gmail.com>
2022-08-21 00:41:57 -0700

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -58,11 +58,11 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("hi asar zia"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
